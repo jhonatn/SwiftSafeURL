f58f7ef5ae5d6e77485b3bcc7e2b30ff08076cdb
Jhonatan A <1548170+baguio@users.noreply.github.com>
2022-07-21 22:27:33 -0500

    Better examples

--- Sources/SafeURLPlayground/main.swift
+++ Sources/SafeURLPlayground/main.swift
@@ -1,6 +1,16 @@
 import Foundation
 import SafeURL
 
-_ = URL(safeString: "")
+// MARK: Valid URLs
+_ = URL(safeString: "127.0.0.1")
+_ = URL(safeString: "localhost")
+_ = URL(safeString: "http://google.com")
+_ = URL(safeString: "https://google.com")
+_ = URL(safeString: "git@github.com:apple/swift.git")
+
+// MARK: Unvalid input
 _ = URL(safeString: "")
 _ = URL(safeString: "bjignuoergnig3j894j8023f444qf@Q#C$T@B^UN&I$")
+
+// MARK: SafeURL requirement error
+_ = URL(safeString: "http://\u{1F600}.test")
