3d36297bd8246c17d331e2d8ab0a34aa8973dd04
Jhonatan A <1548170+baguio@users.noreply.github.com>
2022-08-01 23:36:04 -0500

    Documentation: Fixed mistake in method description

--- Sources/SafeURL/SafeURL.swift
+++ Sources/SafeURL/SafeURL.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 public extension URL {
-    /// Initialize with string. Internally uses  ``Foundation/URL``, `URL.init(string:)`
+    /// Initialize with string. Internally uses `URL.init(string:)`
     ///
     /// Shows an error if a `URL` cannot be formed with the string (for example, if the string contains characters that are illegal in a URL, or is an empty string).
     /// If the file contains the comment `// safeurl:warn`, invalid `URL`s will be compiled, showing a warning instead of an error. Note that this is not recommended, as this will still force-stop the executable on runtime.
