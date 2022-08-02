bf6993e23539552b89abeeceb5f26974d8de9ad8
Jhonatan A <1548170+baguio@users.noreply.github.com>
2022-08-01 23:41:22 -0500

    Testing: Add flag to disable runtime force-closing
    Internal (test) use only. Possibly optimized by the compiler in release binaries

--- Sources/SafeURL/SafeURL.swift
+++ Sources/SafeURL/SafeURL.swift
@@ -1,11 +1,17 @@
 import Foundation
 
+internal var _safeURLRuntimeInTestMode = false
+
 public extension URL {
     /// Initialize with string. Internally uses `URL.init(string:)`
     ///
     /// Shows an error if a `URL` cannot be formed with the string (for example, if the string contains characters that are illegal in a URL, or is an empty string).
     /// If the file contains the comment `// safeurl:warn`, invalid `URL`s will be compiled, showing a warning instead of an error. Note that this is not recommended, as this will still force-stop the executable on runtime.
     init(safeString: StaticString) {
-        self.init(string: "\(safeString)")!
+        if _safeURLRuntimeInTestMode {
+            self.init(string: "localhost")!
+        } else {
+            self.init(string: "\(safeString)")!
+        }
     }
 }
